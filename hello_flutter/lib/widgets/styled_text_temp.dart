be88fa9d506a8c1aa93f65ad65d8efe0b7403e09
tb <darkcrawler.in@gmail.com>
2025-08-22 22:56:32 +0530

    flutter hello app continuation

---
+++ hello_flutter/lib/widgets/styled_text_temp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StyledTextTemp extends StatelessWidget {
+  @override
+  Widget build(context) {
+    // TODO: implement build
+    return const Text(
+      'Hello World',
+      style: TextStyle(color: Colors.white, fontSize: 30),
+    );
+  }
+}
